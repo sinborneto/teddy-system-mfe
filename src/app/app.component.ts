f1e62ea9355613b0321ba15dc60847349741a41d
Sinval Borges <sinborneto@yahoo.com.br>
2025-03-10 17:25:37 -0300

    create microfront system

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { SystemComponent } from "./pages/system/system.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SystemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
