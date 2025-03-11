caba26e930b0c33c54786a1db2b708d463fe71b6
Sinval Borges <sinborneto@yahoo.com.br>
2025-03-11 04:02:51 -0300

    add shared service and pass username in url

--- src/app/pages/system/system.component.ts
+++ src/app/pages/system/system.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { SharedService } from './../../../../../host/src/app/shared/shared.service';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SharedModule } from 'primeng/api';
+
 
 @Component({
   selector: 'app-system',
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './system.component.html',
   styleUrl: './system.component.scss'
 })
-export class SystemComponent {
+export class SystemComponent implements OnInit {
+  private sharedService = inject(SharedService);
+  private route = inject(ActivatedRoute);
 
+  userName = '';
+
+  ngOnInit() {
+    this.getUserName();
+  }
+
+  getUserName() {
+    this.route.paramMap.subscribe((params) => {
+      const nameFromUrl = params.get('name');
+      if (nameFromUrl) {
+        this.userName = nameFromUrl;
+      } else {
+        this.sharedService.currentUser.subscribe((data) => {
+          this.userName = data?.nome || 'Usuário desconhecido';
+        });
+      }
+    });
+  }
 }
